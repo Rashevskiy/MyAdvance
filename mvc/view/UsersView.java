91075dc4cc3daa01b867d6563aa157f7d0622e81
Rashevskiy <50542087+Rashevskiy@users.noreply.github.com>
2020-01-29 19:19:59 +0300

    Add files via upload

---
+++ mvc/view/UsersView.java
@@ -0,0 +1,44 @@
+package mvc.view;
+
+import mvc.controller.Controller;
+import mvc.model.ModelData;
+
+/**
+ * Created by Rumata on 28.02.2017.
+ */
+public class UsersView implements View {
+    private Controller controller;
+
+    public void fireEventShowAllUsers(){
+        controller.onShowAllUsers();
+    }
+
+    public void fireEventShowDeletedUsers() {
+        controller.onShowAllDeletedUsers();
+    }
+
+    public void fireEventOpenUserEditForm(long id) {
+        controller.onOpenUserEditForm(id);
+    }
+
+    @Override
+    public void refresh(ModelData modelData) {
+        if (!modelData.isDisplayDeletedUserList()) {
+            System.out.println("All users:");
+        }
+        if (modelData.isDisplayDeletedUserList()) {
+            System.out.println("All deleted users:");
+        }
+        for (int i = 0; i < modelData.getUsers().size(); i++) {
+            System.out.println("\t" + modelData.getUsers().get(i));
+        }
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+
+}
